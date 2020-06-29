adbba31d4f3f23914701eb1e7f004194f460faeb
ss0506 <ss.abcbe.0506@gmail.com>
2020-06-29 17:48:55 +0530

    Minor changes

--- RotarApp/settings.py
+++ RotarApp/settings.py
@@ -24,7 +24,7 @@ with open('RotarApp/etc/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','www.rotaractrid3131.ml']
 
