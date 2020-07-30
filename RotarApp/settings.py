eb49febeb4f68890b8ff88d2b77c565e2b547b0b
ss0506 <ss.abcbe.0506@gmail.com>
2020-07-31 03:55:08 +0530

    Going live 1.1

--- RotarApp/settings.py
+++ RotarApp/settings.py
@@ -130,6 +130,10 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = '/home/ss0506/RotarApp/static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
