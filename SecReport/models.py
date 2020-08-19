c0f048c6370c463978c691414e50ba6b9ed24f6b
ss0506 <ss.abcbe.0506@gmail.com>
2020-08-19 12:47:30 +0000

    Hotfixes

--- SecReport/models.py
+++ SecReport/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from Auth.models import Account
 import uuid
-from datetime import datetime  
+from datetime import datetime
 
 # # Create your models here.
 
@@ -9,7 +9,7 @@ class Report(models.Model):
     club            = models.ForeignKey(Account, on_delete = models.CASCADE)
     reportId        = models.CharField(max_length=30, verbose_name = "Report Id", primary_key = True)
     month           = models.CharField(max_length=10, verbose_name = "Month")
-    date            = models.DateTimeField(verbose_name = "Reported on", default=datetime.now)
+    date            = models.DateTimeField(verbose_name = "Reported on", default=datetime.now,null=True)
     memberMatrix00  = models.CharField(max_length=4,verbose_name = "Members at the beginning of the month - Male")
     memberMatrix01  = models.CharField(max_length=4,verbose_name = "Members at the beginning of the month - Female")
     memberMatrix02  = models.CharField(max_length=4,verbose_name = "Members at the beginning of the month - Others")
@@ -27,7 +27,7 @@ class Report(models.Model):
     memberMatrix42  = models.CharField(max_length=4,verbose_name = "Guests - Others")
     dues00          = models.CharField(max_length=6,verbose_name = "Dues paid upto last month",default="")
     dues04          = models.CharField(max_length=6,verbose_name = "District Dues paid in this month")
-    bulletin00      = models.CharField(max_length=15,verbose_name = "Name of Bulletin")
+    bulletin00      = models.CharField(max_length=35,verbose_name = "Name of Bulletin")
     bulletin01      = models.CharField(max_length=10,verbose_name = "Type of Bulletin")
     bulletin02      = models.CharField(max_length=100,verbose_name = "Link")
     bulletin03      = models.DateTimeField(verbose_name = "Issued on", null=True)
@@ -43,7 +43,7 @@ class Report(models.Model):
         verbose_name = 'Report'
         verbose_name_plural = 'Reports'
         get_latest_by = 'date'
-    
+
     def __str__(self):
         return f'{self.club.name}-{self.month}'
 
@@ -51,8 +51,8 @@ class GBM(models.Model):
     club    = models.ForeignKey(Account, on_delete = models.CASCADE)
     reportId= models.ForeignKey(Report, on_delete = models.CASCADE)
     gbm0    = models.CharField(max_length=2,verbose_name = "Meeting Number")
-    gbm1    = models.DateTimeField(verbose_name = "Date", default=datetime.now)
-    gbm2    = models.CharField(max_length=100,verbose_name = "Agenda")
+    gbm1    = models.DateTimeField(verbose_name = "Date", default=datetime.now, null=True)
+    gbm2    = models.CharField(max_length=700,verbose_name = "Agenda")
     gbm3    = models.BooleanField(verbose_name="Bylaws passed?")
     gbm4    = models.BooleanField(verbose_name="Budget passed?")
     gbm5    = models.CharField(max_length=4,verbose_name = "Attendance")
@@ -60,7 +60,7 @@ class GBM(models.Model):
     class Meta:
         verbose_name = 'GBM'
         verbose_name_plural = 'GBMs'
-    
+
     def __str__(self):
         return f'{self.club.name}-{self.reportId.month}-{self.gbm0}'
 
@@ -68,8 +68,8 @@ class BOD(models.Model):
     club    = models.ForeignKey(Account, on_delete = models.CASCADE)
     reportId= models.ForeignKey(Report, on_delete = models.CASCADE)
     bod0    = models.CharField(max_length=2,verbose_name = "Meeting Number")
-    bod1    = models.DateTimeField(verbose_name = "Date", default=datetime.now)
-    bod2    = models.CharField(max_length=100,verbose_name = "Agenda")
+    bod1    = models.DateTimeField(verbose_name = "Date", default=datetime.now, null=True)
+    bod2    = models.CharField(max_length=700,verbose_name = "Agenda")
     bod3    = models.BooleanField(verbose_name="Bylaws passed?")
     bod4    = models.BooleanField(verbose_name="Budget passed?")
     bod5    = models.CharField(max_length=4,verbose_name = "Attendance")
@@ -77,35 +77,35 @@ class BOD(models.Model):
     class Meta:
         verbose_name = 'BOD'
         verbose_name_plural = 'BODs'
-    
+
     def __str__(self):
         return f'{self.club.name}-{self.reportId.month}-{self.bod0}'
 
 class Event(models.Model):
     club        = models.ForeignKey(Account, on_delete = models.CASCADE)
     reportId    = models.ForeignKey(Report, on_delete = models.CASCADE)
-    event0      = models.DateTimeField(verbose_name = "Date", default=datetime.now)
-    event1      = models.CharField(max_length=15,verbose_name = "Name")
+    event0      = models.DateTimeField(verbose_name = "Date", default=datetime.now, null=True)
+    event1      = models.CharField(max_length=35,verbose_name = "Name")
     event2      = models.CharField(max_length=3,verbose_name = "Avenue")
     event3      = models.CharField(max_length=5,verbose_name = "Attendance")
     event4      = models.CharField(max_length=5,verbose_name = "Volunteer Hours")
     event5      = models.CharField(max_length=7,verbose_name = "Funds Raised")
-    event6      = models.CharField(max_length=100,verbose_name = "Description")
+    event6      = models.CharField(max_length=700,verbose_name = "Description")
     event7      = models.CharField(max_length=100,verbose_name = "Instagram Link")
-    
+
     class Meta:
         verbose_name = 'Event'
         verbose_name_plural = 'Events'
-    
+
     def __str__(self):
         return f'{self.club.name}-{self.event1}'
 
 class FutureEvent(models.Model):
     club            = models.ForeignKey(Account, on_delete = models.CASCADE)
     reportId        = models.ForeignKey(Report, on_delete = models.CASCADE)
-    futureEvent0     = models.DateTimeField(verbose_name = "Date", default=datetime.now)
-    futureEvent1     = models.CharField(max_length=15,verbose_name = "Name")
-    futureEvent2     = models.CharField(max_length=100,verbose_name = "Description")
+    futureEvent0     = models.DateTimeField(verbose_name = "Date", default=datetime.now, null=True)
+    futureEvent1     = models.CharField(max_length=35,verbose_name = "Name")
+    futureEvent2     = models.CharField(max_length=700,verbose_name = "Description")
     futureEvent3     = models.CharField(max_length=3,verbose_name = "Avenue")
 
     class Meta:
--- SecReport/views.py
+++ SecReport/views.py
@@ -43,7 +43,7 @@ def month(string) :
 @login_required
 
 def report(request):
-    
+
     currentMonth = datetime.now().month
     month = "0" + str(int(currentMonth)-1) if (currentMonth!=1 and currentMonth<11) else ("12" if currentMonth==1 else str(int(currentMonth)-1))
     print("currentMonth"+str(currentMonth),end=" ")
@@ -55,119 +55,126 @@ def report(request):
         profile = Club.objects.filter(account=request.user).first()
 
         if Report.objects.filter(reportId=reportId).exists():
-            return render(request, 'SecReport/reportFoundResponse.html',{'title':'Reporting','tab':'report','reportId':reportId,'profile':profile})        
+            return render(request, 'SecReport/reportFoundResponse.html',{'title':'Reporting','tab':'report','reportId':reportId,'profile':profile})
 
-        else :            
-            if(request.method == 'POST'):
-            
-                try :
-                    reportId = str(request.user.rotaryId)+"-"+request.POST['month']
-                    context=dict()
-                    contextList = []
-                    context['profile']=profile
-                    
-                    for x in request.POST :
-            
-                        context[x] = request.POST[x]
-                        contextList.append(x)
-            
-                    # Actual loading
-                    with transaction.atomic():
+        else :
 
-                        report = Report(club = request.user,
-                                    month           = (request.POST['month']),
-                                    reportId        = reportId,
-                                    memberMatrix00  = request.POST['memberMatrix00'],
-                                    memberMatrix01  = request.POST['memberMatrix01'],
-                                    memberMatrix02  = request.POST['memberMatrix02'],
-                                    memberMatrix10  = request.POST['memberMatrix10'],
-                                    memberMatrix11  = request.POST['memberMatrix11'],
-                                    memberMatrix12  = request.POST['memberMatrix12'],
-                                    memberMatrix20  = request.POST['memberMatrix20'],
-                                    memberMatrix21  = request.POST['memberMatrix21'],
-                                    memberMatrix22  = request.POST['memberMatrix22'],
-                                    memberMatrix30  = request.POST['memberMatrix30'],
-                                    memberMatrix31  = request.POST['memberMatrix31'],
-                                    memberMatrix32  = request.POST['memberMatrix32'],
-                                    memberMatrix40  = request.POST['memberMatrix40'],
-                                    memberMatrix41  = request.POST['memberMatrix41'],
-                                    memberMatrix42  = request.POST['memberMatrix42'],
-                                    dues00          = request.POST['dues00'],
-                                    dues04          = request.POST['dues04'],
-                                    bulletin00      = request.POST['bulletin00'],
-                                    bulletin01      = request.POST['bulletin01'],
-                                    bulletin02      = request.POST['bulletin02'],
-                                    bulletin03      = request.POST['bulletin03'] if request.POST['bulletin03'] else None,
-                                    bulletin04      = request.POST['bulletin04'] if request.POST['bulletin04'] else None,
-                                    bulletin05      = request.POST['bulletin05'],
-                                    feedback00      = request.POST['feedback00'],
-                                    feedback01      = request.POST['feedback01'],
-                                    feedback02      = request.POST['feedback02'],
-                                    feedback03      = request.POST['feedback03'],
-                                    suggestion00    = request.POST['suggestion00']
-                                ) 
-                        report.save()  
+            if request.user.rotaryId == 214097 :
 
-                        gbmlist = request.POST['gbmlist'].split(',')
-                        for x in gbmlist :
-                            gbm = GBM.objects.create(
+                if(request.method == 'POST'):
+
+                    try :
+                        reportId = str(request.user.rotaryId)+"-"+request.POST['month']
+                        context=dict()
+                        contextList = []
+                        context['profile']=profile
+
+                        for x in request.POST :
+
+                            context[x] = request.POST[x]
+                            contextList.append(x)
+
+                        # Actual loading
+                        with transaction.atomic():
+
+                            report = Report(club = request.user,
+                                        month           = (request.POST['month']),
+                                        reportId        = reportId,
+                                        memberMatrix00  = request.POST['memberMatrix00'],
+                                        memberMatrix01  = request.POST['memberMatrix01'],
+                                        memberMatrix02  = request.POST['memberMatrix02'],
+                                        memberMatrix10  = request.POST['memberMatrix10'],
+                                        memberMatrix11  = request.POST['memberMatrix11'],
+                                        memberMatrix12  = request.POST['memberMatrix12'],
+                                        memberMatrix20  = request.POST['memberMatrix20'],
+                                        memberMatrix21  = request.POST['memberMatrix21'],
+                                        memberMatrix22  = request.POST['memberMatrix22'],
+                                        memberMatrix30  = request.POST['memberMatrix30'],
+                                        memberMatrix31  = request.POST['memberMatrix31'],
+                                        memberMatrix32  = request.POST['memberMatrix32'],
+                                        memberMatrix40  = request.POST['memberMatrix40'],
+                                        memberMatrix41  = request.POST['memberMatrix41'],
+                                        memberMatrix42  = request.POST['memberMatrix42'],
+                                        dues00          = request.POST['dues00'],
+                                        dues04          = request.POST['dues04'],
+                                        bulletin00      = request.POST['bulletin00'],
+                                        bulletin01      = request.POST['bulletin01'],
+                                        bulletin02      = request.POST['bulletin02'],
+                                        bulletin03      = request.POST['bulletin03'] if request.POST['bulletin03'] else None,
+                                        bulletin04      = request.POST['bulletin04'] if request.POST['bulletin04'] else None,
+                                        bulletin05      = request.POST['bulletin05'],
+                                        feedback00      = request.POST['feedback00'],
+                                        feedback01      = request.POST['feedback01'],
+                                        feedback02      = request.POST['feedback02'],
+                                        feedback03      = request.POST['feedback03'],
+                                        suggestion00    = request.POST['suggestion00']
+                                    )
+                            report.save()
+
+                            gbmlist = request.POST['gbmlist'].split(',')
+                            for x in gbmlist :
+                                gbm = GBM.objects.create(
+                                    club = request.user,
+                                    reportId = report,
+                                    gbm0 = request.POST[x+"-0"],
+                                    gbm1 = request.POST[x+"-1"] if request.POST[x+"-1"] else None,
+                                    gbm2 = request.POST[x+"-2"],
+                                    gbm3 = request.POST[x+"-3"],
+                                    gbm4 = request.POST[x+"-4"],
+                                    gbm5 = request.POST[x+"-5"]
+                                )
+
+                            bodlist = request.POST['bodlist'].split(',')
+                            for x in bodlist :
+                                bod = BOD.objects.create(
+                                    club = request.user,
+                                    reportId = report,
+                                    bod0 = request.POST[x+"-0"],
+                                    bod1 = request.POST[x+"-1"] if request.POST[x+"-1"] else None ,
+                                    bod2 = request.POST[x+"-2"],
+                                    bod3 = request.POST[x+"-3"],
+                                    bod4 = request.POST[x+"-4"],
+                                    bod5 = request.POST[x+"-5"]
+                                )
+
+                            eventlist = request.POST['eventlist'].split(',')
+                            for x in eventlist :
+                                event = Event.objects.create(
+                                    club = request.user,
+                                    reportId = report,
+                                    event0 = request.POST[x+"-0"] if request.POST[x+"-0"] else None,
+                                    event1 = request.POST[x+"-1"],
+                                    event2 = request.POST[x+"-2"],
+                                    event3 = request.POST[x+"-3"],
+                                    event4 = request.POST[x+"-4"],
+                                    event5 = request.POST[x+"-5"],
+                                    event6 = request.POST[x+"-6"],
+                                    event7 = request.POST[x+"-7"]
+                                )
+
+                            futureEventlist = request.POST['futureEventlist'].split(',')
+                            for x in futureEventlist :
+                                futureEvent = FutureEvent.objects.create(
                                 club = request.user,
                                 reportId = report,
-                                gbm0 = request.POST[x+"-0"],
-                                gbm1 = request.POST[x+"-1"],
-                                gbm2 = request.POST[x+"-2"],
-                                gbm3 = request.POST[x+"-3"],
-                                gbm4 = request.POST[x+"-4"],
-                                gbm5 = request.POST[x+"-5"]
+                                futureEvent0 = request.POST[x+"-0"] if request.POST[x+"-0"] else None,
+                                futureEvent1 = request.POST[x+"-1"],
+                                futureEvent2 = request.POST[x+"-2"],
+                                futureEvent3 = request.POST[x+"-3"],
                             )
-                        
-                        bodlist = request.POST['bodlist'].split(',')
-                        for x in bodlist :
-                            bod = BOD.objects.create(
-                                club = request.user,
-                                reportId = report,
-                                bod0 = request.POST[x+"-0"],
-                                bod1 = request.POST[x+"-1"],
-                                bod2 = request.POST[x+"-2"],
-                                bod3 = request.POST[x+"-3"],
-                                bod4 = request.POST[x+"-4"],
-                                bod5 = request.POST[x+"-5"]
-                            )
-                        
-                        eventlist = request.POST['eventlist'].split(',')
-                        for x in eventlist :
-                            event = Event.objects.create(
-                                club = request.user,
-                                reportId = report,
-                                event0 = request.POST[x+"-0"],
-                                event1 = request.POST[x+"-1"],
-                                event2 = request.POST[x+"-2"],
-                                event3 = request.POST[x+"-3"],
-                                event4 = request.POST[x+"-4"],
-                                event5 = request.POST[x+"-5"],
-                                event6 = request.POST[x+"-6"],
-                                event7 = request.POST[x+"-7"]
-                            )
-                        
-                        futureEventlist = request.POST['futureEventlist'].split(',')
-                        for x in futureEventlist :
-                            futureEvent = FutureEvent.objects.create(
-                            club = request.user,
-                            reportId = report,
-                            futureEvent0 = request.POST[x+"-0"],
-                            futureEvent1 = request.POST[x+"-1"],
-                            futureEvent2 = request.POST[x+"-2"],
-                            futureEvent3 = request.POST[x+"-3"],
-                        )
-            
-                    return render(request, 'SecReport/paginatedReportResponse.html',{'title':'Reporting','tab':'report','success':'The report has been saved successfully','reportId':report.reportId,'profile':profile})        
-            
-                except Exception as e:
-                    print(e)
-                    return render(request, 'SecReport/paginatedReport.html',{'contextDict':context,'title':'Reporting','tab':'report','profile':profile, 'error':e})        
+
+                        return render(request, 'SecReport/paginatedReportResponse.html',{'title':'Reporting','tab':'report','success':'The report has been saved successfully','reportId':report.reportId,'profile':profile})
+
+                    except Exception as e:
+                        print(e)
+                        return render(request, 'SecReport/paginatedReport.html',{'contextDict':context,'title':'Reporting','tab':'report','profile':profile, 'error':e})
+                else :
+                    return render(request, 'SecReport/paginatedReport.html',{'title':'Reporting','tab':'report','profile':profile})
+
             else :
-                return render(request, 'SecReport/paginatedReport.html',{'title':'Reporting','tab':'report','profile':profile})
-    
+                return render(request, 'SecReport/deadlineMissed.html',{'title':'Reporting','tab':'report','reportId':reportId,'profile':profile})
+
+
     elif request.user == Account.objects.filter(username='admin').first() :
         Clubs = Account.objects.filter(is_club = True).all()
         Reports = Report.objects.all()
@@ -251,7 +258,7 @@ def exportReport(request,reportid):
         row_num += 1
         for col_num in range(len(row)):
             ws.write(row_num, col_num, str(row[col_num]), font_style)
-    
+
     #Event
     ws = wb.add_sheet('Event')
     row_num = 0
@@ -267,7 +274,7 @@ def exportReport(request,reportid):
         for col_num in range(len(row)):
             ws.write(row_num, col_num, str(row[col_num]), font_style)
 
-    # FutureEvent    
+    # FutureEvent
     ws = wb.add_sheet('FutureEvent')
     row_num = 0
     font_style = xlwt.XFStyle()
